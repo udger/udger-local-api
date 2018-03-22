8de6d00d79394c234048517aec4616f2bf39fd44
skybber <lada.dvorak7@gmail.com>
2018-03-22 22:37:28 +0100

    Add simple ping resource

---
+++ src/main/java/org/udger/restapi/resource/PingResource.java
@@ -0,0 +1,24 @@
+package org.udger.restapi.resource;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.udger.restapi.service.ParserStatistics;
+
+@Path("ping")
+public class PingResource {
+
+    @Inject
+    private ParserStatistics parserStatistics;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getStatistic() {
+        parserStatistics.updateStatisticUA(0);
+        return Response.ok().build();
+    }
+}
