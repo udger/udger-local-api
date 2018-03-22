553c680b07eb8f0c7b316f668de3cd4b2e9b82d5
skybber <lada.dvorak7@gmail.com>
2018-03-22 22:36:46 +0100

    Improve statistic time resolution

--- src/main/java/org/udger/restapi/service/ParserStatistics.java
+++ src/main/java/org/udger/restapi/service/ParserStatistics.java
@@ -17,19 +17,21 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class ParserStatistics {
 
-    private static final int AVG_PERIOD = 10;
+    private static final int BUCKETS_SECS = 10;
+    private static final int BUCKETS_PER_SECS = 4;
+    private static final int TOTAL_BUCKETS = BUCKETS_SECS * BUCKETS_PER_SECS;
 
     private static class StatisticRec {
 
-        private int counterChunks[] = new int[AVG_PERIOD];
+        private int buckets[] = new int[TOTAL_BUCKETS];
 
         private int totalRequests;
         private long totalNonos;
-        private long lastSec;
+        private long lastBucket;
 
         public StatisticRec() {
-            for (int i = 0; i < AVG_PERIOD; i++) {
-                counterChunks[i] = 0;
+            for (int i = 0; i < TOTAL_BUCKETS; i++) {
+                buckets[i] = 0;
             }
         }
     }
@@ -90,19 +92,15 @@ public class ParserStatistics {
     }
 
     private synchronized double doGetAvgThroughput(StatisticRec stat) {
-        if (stat.lastSec > 0) {
-            long curSec = System.currentTimeMillis() / 1000;
-            shuffleChunksToSec(stat, curSec);
-
-            // don't count last chunk since it is not completed
-            long indx = stat.lastSec;
+        if (stat.lastBucket > 0) {
+            long curBucket = System.currentTimeMillis() * BUCKETS_PER_SECS / 1000;
+            shuffleBucket(stat, curBucket);
 
             int tc = 0;
-            for (int i = AVG_PERIOD; i > 0; i--) {
-                tc += stat.counterChunks[(int) indx % AVG_PERIOD];
-                indx --;
+            for (int i = 0; i < TOTAL_BUCKETS; i++) {
+                tc += stat.buckets[i];
             }
-            return Math.round(100.0 * tc / (AVG_PERIOD - 0.5)) / 100.0;
+            return Math.round(100.0 * tc / (BUCKETS_SECS - 0.5)) / 100.0;
         }
         return 0.0;
     }
@@ -130,22 +128,21 @@ public class ParserStatistics {
         stat.totalRequests ++;
         stat.totalNonos += nanos;
 
-        long curSec = System.currentTimeMillis() / 1000;
-        shuffleChunksToSec(stat, curSec);
-        stat.counterChunks[(int) curSec % AVG_PERIOD] ++;
+        long curBucket = System.currentTimeMillis() * BUCKETS_PER_SECS / 1000;
+        shuffleBucket(stat, curBucket);
+        stat.buckets[(int) (curBucket % TOTAL_BUCKETS)] ++;
     }
 
-    private void shuffleChunksToSec(StatisticRec stat, long curSec) {
-        if (curSec != stat.lastSec) {
-            long indx = stat.lastSec + 1;
-            if (curSec - (AVG_PERIOD - 1) > indx) {
-                indx = curSec - (AVG_PERIOD - 1);
+    private void shuffleBucket(StatisticRec stat, long bucket) {
+        if (bucket != stat.lastBucket) {
+            long indx = stat.lastBucket + 1;
+            if (bucket - (TOTAL_BUCKETS - 1) > indx) {
+                indx = bucket - (TOTAL_BUCKETS - 1);
             }
-            while (indx <= curSec) {
-                stat.counterChunks[(int) indx % AVG_PERIOD] = 0;
-                indx ++;
+            for(;indx <= bucket;indx++) {
+                stat.buckets[(int) (indx % TOTAL_BUCKETS)] = 0;
             }
-            stat.lastSec = curSec;
+            stat.lastBucket = bucket;
         }
     }
 }
